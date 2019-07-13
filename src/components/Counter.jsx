d3e9067b0c23d2c95f1ddf03b9debf0c398069fa
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-07-13 08:40:47 -0700

    add sellPint method

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
+function Counter({ kegLevel }) {
+
+  let [count, setCount] = useState(kegLevel);
+
+  function sellPint() {
+    if (count < 0) {
+      ;
+    } else {
+      setCount - 1;
+    }
+  }
+  return (
+    <div>
+      <h2>h2ints Remaining: {kegLevel}</h2>
+      <button onClick={sellPint}>Sell Pint</button>
+    </div>
+  )
+}
